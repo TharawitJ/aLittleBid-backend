163c013afc94b9f9e7a8f028f942d49b99199925
tingchannavong <tingchannavong.gmail.com>
2026-04-20 14:16:45 +0700

    feat: add registration endpoint and update OpenAPI documentation for login and registration

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,7 +5,45 @@ import { login, register } from "../controllers/auth.controllers.js";
 
 const authRoute = express.Router();
 
+
+/**
+ * @openapi
+ * /auth/login:
+ *   post:
+ *     summary: Login to a little bid
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/Login'
+ *     responses:
+ *       200:
+ *         description: Log in successfully
+ *       404:
+ *         description: Fail to add log in
+ */
 authRoute.post("/login", login);
+
+/**
+ * @openapi
+ * /auth/register:
+ *   post:
+ *     summary: Register to a little bid
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/Register'
+ *     responses:
+ *       200:
+ *         description: Registered successfully
+ *       404:
+ *         description: Fail to add Register
+ */
 authRoute.post("/register", register);
 
 // ส่วนของ OTP / Forgot Password
