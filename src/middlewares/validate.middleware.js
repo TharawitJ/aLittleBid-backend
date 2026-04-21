29c12d1e494533b12cd462c4b4b9288c2c110aa9
tingchannavong <tingchannavong.gmail.com>
2026-04-21 17:27:26 +0700

    feat: enhance error handling and validation middleware with improved logging and error propagation

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -5,7 +5,8 @@ export const validate = (schema, target = "body") =>
   (req, res, next) => {
     const result = schema.safeParse(req[target])
     if (!result.success) {
-      return res.status(400).json({ errors: result.error.flatten().fieldErrors })
+      console.log("--- Validation Failed at zod---");
+      return next(result.error);
     }
     req[target] = result.data  
     next()
