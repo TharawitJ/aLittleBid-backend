207dd8415e51f35ad71f7271880ed4cd8a65eb98
tingchannavong <tingchannavong.gmail.com>
2026-04-22 13:47:34 +0700

    getauction id include bids in desc order

--- src/services/auction.service.js
+++ src/services/auction.service.js
@@ -42,7 +42,11 @@ export async function getAllAuctions() {
 export async function getAuctionById(id) {
   const result = await prisma.auction.findUnique({
     where: { id },
-    include: { bids: true }
+    include: { bids: {
+      orderBy: {
+        amount: 'desc',
+      }
+    } }
   });
   if (!result) throw createError(404, "Invalid auction");
   return result;
